7f4a9630e766cbdfd02f15d928976b76fec26cb0
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-23 12:44:38 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { Container } from '@chakra-ui/react'
 import { Routes, Route } from 'react-router-dom'
+import UserPage from './components/UserPage'
+import PostPage from './pages/PostPage'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <section>
       <Container maxW='620px'>
-      <Routes>
-        <Route path='/:username' element={<Home />} />
-      </Routes>
+        <Header />
+        <Routes>
+          <Route path='/:username' element={<UserPage />} />
+          <Route path='/:username/post/:pid' element={<PostPage />} />
+        </Routes>
       </Container>
     </section>
   )
