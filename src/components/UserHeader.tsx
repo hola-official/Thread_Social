1b88e33405a5722180d7c779d5110018f0c71ef3
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-23 14:27:42 +0100

    commit

--- src/components/UserHeader.tsx
+++ src/components/UserHeader.tsx
@@ -1,8 +1,12 @@
-import { VStack } from '@chakra-ui/layout'
+import { VStack, Flex } from '@chakra-ui/layout'
 import React from 'react'
 
 const UserHeader = () => {
-  return <VStack></VStack>
+  return <VStack gap={4} alignItems={'start'}>
+    <Flex justifyContent={'space-between'} w={'full'}>
+        
+    </Flex>
+  </VStack>
 }
 
 export default UserHeader
