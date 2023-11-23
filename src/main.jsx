704607ecb290afc14dfdb9909bc193f1ce5a3ce7
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-23 11:23:42 +0100

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import { mode } from '@chakra-ui/theme-tools'
+
+const style = {
+  global: (props) => ({
+    body: {
+      color: mode('gray.800', 'whiteAlpha:900')(props),
+      bg: mode('gray.100', '#101010')(props)
+    }
+  })
+}
+
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: true,
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
