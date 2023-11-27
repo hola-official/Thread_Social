a3c1ab4cc3067d1f1e303c1cf95011535eab1795
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-27 10:41:30 +0100

    commit

--- src/components/UserPosts.tsx
+++ src/components/UserPosts.tsx
@@ -6,9 +6,9 @@ const UserPosts = () => {
     return (
         <Link to={"/:aliumusa/post/:1"}>
             <Flex gap={3} mb={4} py={5}>
-                <Flex>
+                <Flex >
                     <Avatar src="/post1.png" size={"md"} name="Mark Zuckerberg" />
-                    <Box w={1} h={"full"} bg={'gray.light'} my={2}></Box>
+                    <Box w={'1px'} h={"full"} bg={'gray.light'} my={2}></Box>
                 </Flex>
             </Flex>
         </Link>
