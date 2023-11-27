0eb17063c0666c602933c8340bb5b6387eb29f77
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-27 09:54:28 +0100

    commit

--- src/components/UserPosts.tsx
+++ src/components/UserPosts.tsx
@@ -2,11 +2,8 @@ import { Link } from 'react-router-dom'
 import React from 'react'
 
 const UserPosts = () => {
-  return (
-    <>
-    <Link to={}></Link>
-    </>
-  )
+  return <Link to={'/:aliumusa/post/:1'}></Link>
+
 }
 
 export default UserPosts
--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import UserHeader from '../components/UserHeader'
+import UserPosts from '../components/UserPosts'
 
 const UserPage = () => {
   return (
     <>
       <UserHeader />
+      <UserPosts />
     </>
   )
 }
