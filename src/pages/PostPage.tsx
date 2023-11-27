25f93eb357dd3f14cda333e3927c43500cffe13a
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-27 15:09:01 +0100

    commit

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -1,23 +1,27 @@
 import React from 'react'
 import { BsThreeDots } from "react-icons/bs";
-import { Avatar, Flex, Image, Text } from '@chakra-ui/react'
+import { Avatar, Box, Flex, Image, Text } from '@chakra-ui/react'
 
 const PostPage = () => {
   return (
     <>
-        <Flex w={'full'} gap={3} alignItems={'center'} justifyContent={'center'}>
-          <Avatar src='/aliumusa.jpeg' name='Aliu Musa' size={'md'} />
-          
-          <Flex alignItems={'center'}>
-            <Text fontSize={'sm'}>Aliu Musa</Text>
-            <Image src='/verified.png' h={4} w={4} ml={2} />
-          </Flex>
+      <Flex w={'full'} gap={3} alignItems={'center'} justifyContent={'space-between'}>
 
-          <Flex alignItems={'center'} gap={4}>
-            <Text fontSize={{base: "xs", md: "sm"}} textAlign={'right'} w={36}color={'gray.light'} >2day</Text>
-            <BsThreeDots cursor={'pointer'} />
-          </Flex>
+        <Flex alignItems={'center'}>
+          <Avatar src='/aliumusa.jpeg' name='Aliu Musa' size={'md'} mr={2} />
+          <Text fontSize={'sm'}>Aliu Musa</Text>
+          <Image src='/verified.png' h={4} w={4} ml={2} />
         </Flex>
+
+        <Flex alignItems={'center'} gap={4}>
+          <Text fontSize={{ base: "xs", md: "sm" }} textAlign={'right'} color={'gray.light'} >2day</Text>
+          <BsThreeDots cursor={'pointer'} />
+        </Flex>
+      </Flex>
+      <Text my={3}>Hello gbogbo aye!!!</Text>
+      <Box borderRadius={6} overflow={'hidden'} border={'1px solid'} borderColor={'gray.light'}>
+        <Image src='/post1.png' width={'full'} />
+      </Box>
     </>
   )
 }
