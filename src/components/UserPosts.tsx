7f916f86387b049907eeaa08c962f7014d5edddb
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-27 09:58:50 +0100

    commit

--- src/components/UserPosts.tsx
+++ src/components/UserPosts.tsx
@@ -1,8 +1,11 @@
-import { Link } from 'react-router-dom'
 import React from 'react'
+import { Flex } from '@chakra-ui/layout'
+import { Link } from 'react-router-dom'
 
 const UserPosts = () => {
-  return <Link to={'/:aliumusa/post/:1'}></Link>
+  return <Link to={'/:aliumusa/post/:1'}>
+    <Flex></Flex>
+  </Link>
 
 }
 
