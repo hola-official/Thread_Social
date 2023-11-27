5047ab924302cf735f98fa5fcb315dc16637af23
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-27 11:04:37 +0100

    commit

--- src/components/UserPosts.tsx
+++ src/components/UserPosts.tsx
@@ -1,38 +1,53 @@
 import React from "react";
-import { Flex, Avatar, Box } from "@chakra-ui/react";
+import { Flex, Avatar, Box, Image, Text } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 const UserPosts = () => {
-  return (
-    <Link to={"/:aliumusa/post/:1"}>
-      <Flex gap={3} mb={4} py={5}>
-        <Flex flexDir={"column"}>
-          <Avatar src="/post1.png" size={"md"} name="Mark Zuckerberg" />
-          <Box w={"1px"} h={"full"} bg={"gray.light"} my={2}></Box>
-          <Box pos={"relative"} w={"full"}>
-            <Avatar
-              src="/post1.png"
-              size={"sm"}
-              name="Wale Oloyin"
-              pos={"absolute"}
-            />
-            <Avatar
-              src="/post1.png"
-              size={"sm"}
-              name="Bella Sh,urda"
-              pos={"absolute"}
-            />
-            <Avatar
-              src="/post1.png"
-              size={"sm"}
-              name="Mark Danfo Driver"
-              pos={"absolute"}
-            />
-          </Box>
-        </Flex>
-      </Flex>
-    </Link>
-  );
+    return (
+        <Link to={"/:aliumusa/post/:1"}>
+            <Flex gap={3} mb={4} py={5}>
+                <Flex flexDir={"column"}>
+                    <Avatar src="/post1.png" size={"md"} name="Mark Zuckerberg" />
+                    <Box w={"1px"} h={"full"} bg={"gray.light"} my={2}></Box>
+                    <Box pos={"relative"} w={"full"}>
+                        <Avatar
+                            src="/post1.png"
+                            size={"xs"}
+                            name="Wale Oloyin"
+                            pos={"absolute"}
+                            top={'0'}
+                            left={'15px'}
+                            padding={'2px'}
+                        />
+                        <Avatar
+                            src="/post1.png"
+                            size={"xs"}
+                            name="Bella Sh,urda"
+                            pos={"absolute"}
+                            bottom={'0'}
+                            right={'-5px'}
+                            padding={'2px'}
+                        />
+                        <Avatar
+                            src="/post1.png"
+                            size={"xs"}
+                            name="Mark Danfo Driver"
+                            pos={"absolute"}
+                            bottom={'0'}
+                            left={'4px'}
+                            padding={'2px'}
+                        />
+                    </Box>
+                </Flex>
+                <Flex flex={1} flexDir={'column'} w={'full'}>
+                    <Flex>
+                        <Text>Aliu Musa</Text>
+                        <Image></Image>
+                    </Flex>
+                </Flex>
+            </Flex>
+        </Link>
+    );
 };
 
 export default UserPosts;
