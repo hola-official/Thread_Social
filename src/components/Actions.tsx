900acb2da1a06dad796338b40f803729e9c47019
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-27 14:46:00 +0100

    commit

--- src/components/Actions.tsx
+++ src/components/Actions.tsx
@@ -19,7 +19,7 @@ const Actions = ({ liked, setLiked }) => {
           <path
             d="M1 7.66c0 4.575 3.899 9.086 9.987 12.934.338.203.74.406 1.013.406.283 0 .686-.203 1.013-.406C19.1 16.746 23 12.234 23 7.66 23 3.736 20.245 1 16.672 1 14.603 1 12.98 1.94 12 3.352 11.042 1.952 9.408 1 7.328 1 3.766 1 1 3.736 1 7.66Z"
             stroke="currentColor"
-            stroke-width="2"
+            strokeWidth="2"
           ></path>
         </svg>
 
@@ -36,8 +36,8 @@ const Actions = ({ liked, setLiked }) => {
             d="M20.656 17.008a9.993 9.993 0 1 0-3.59 3.615L22 22Z"
             fill="none"
             stroke="currentColor"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinejoin="round"
+            strokeWidth={"2"}
           ></path>
         </svg>
 
@@ -65,8 +65,8 @@ const Actions = ({ liked, setLiked }) => {
           <line
             fill="none"
             stroke="currentColor"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinejoin="round"
+            stroke-width={"2"}
             x1="22"
             x2="9.218"
             y1="3"
@@ -76,8 +76,8 @@ const Actions = ({ liked, setLiked }) => {
             fill="none"
             points="11.698 20.334 22 3.001 2 3.001 9.218 10.084 11.698 20.334"
             stroke="currentColor"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinejoin="round"
+            strokeWidth={"2"}
           ></polygon>
         </svg>
       </Flex>
--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -1,8 +1,19 @@
+import { Avatar, Flex, Image, Text } from '@chakra-ui/react'
 import React from 'react'
 
 const PostPage = () => {
   return (
-    <div>PostPage</div>
+    <>
+      <Flex>
+        <Flex w={'full'} gap={3} alignItems={'center'}>
+          <Avatar src='/aliumusa.jpeg' name='Aliu Musa' size={'md'} />
+          <Flex alignItems={'center'} gap={1}>
+          <Text>Aliu Musa</Text>
+          <Image src='/verified.png' h={4} w={4}/>
+          </Flex>
+        </Flex>
+      </Flex>
+    </>
   )
 }
 
