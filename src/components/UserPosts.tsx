9a73253c47f0905171b1a8f025d8f06dc8651857
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-27 10:13:17 +0100

    commit

--- src/components/UserPosts.tsx
+++ src/components/UserPosts.tsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import { Flex, Avatar } from '@chakra-ui/react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Flex, Avatar } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 const UserPosts = () => {
-    return <Link to={'/:aliumusa/post/:1'}>
-        <Flex gap={3} mb={4} py={5}>
-            <Avatar src='/directorPro.jpeg' />
-        </Flex>
+  return (
+    <Link to={"/:aliumusa/post/:1"}>
+      <Flex gap={3} mb={4} py={5}>
+        <Avatar src="/post1.png" size={"md"} name="Mark Zuckerberg" />
+      </Flex>
     </Link>
+  );
+};
 
-}
-
-export default UserPosts
+export default UserPosts;
