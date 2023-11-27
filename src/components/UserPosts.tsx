ed9ace8f17272ce046c796c782513f7b24bbd253
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-27 10:50:10 +0100

    commit

--- src/components/UserPosts.tsx
+++ src/components/UserPosts.tsx
@@ -3,21 +3,36 @@ import { Flex, Avatar, Box } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 const UserPosts = () => {
-    return (
-        <Link to={"/:aliumusa/post/:1"}>
-            <Flex gap={3} mb={4} py={5}>
-                <Flex flexDir={'column'}>
-                    <Avatar src="/post1.png" size={"md"} name="Mark Zuckerberg" />
-                    <Box w={'1px'} h={"full"} bg={'gray.light'} my={2}></Box>
-                    <Box pos={'relative'} w={'full'}>
-                        <Avatar src="/post1.png" size={"md"} name="Mark Zuckerberg" />
-                        <Avatar src="/post1.png" size={"md"} name="Mark Zuckerberg" />
-                        <Avatar src="/post1.png" size={"md"} name="Mark Zuckerberg" />
-                    </Box>
-                </Flex>
-            </Flex>
-        </Link>
-    );
+  return (
+    <Link to={"/:aliumusa/post/:1"}>
+      <Flex gap={3} mb={4} py={5}>
+        <Flex flexDir={"column"}>
+          <Avatar src="/post1.png" size={"md"} name="Mark Zuckerberg" />
+          <Box w={"1px"} h={"full"} bg={"gray.light"} my={2}></Box>
+          <Box pos={"relative"} w={"full"}>
+            <Avatar
+              src="/post1.png"
+              size={"sm"}
+              name="Wale Oloyin"
+              pos={"absolute"}
+            />
+            <Avatar
+              src="/post1.png"
+              size={"sm"}
+              name="Bella Sh,urda"
+              pos={"absolute"}
+            />
+            <Avatar
+              src="/post1.png"
+              size={"sm"}
+              name="Mark Danfo Driver"
+              pos={"absolute"}
+            />
+          </Box>
+        </Flex>
+      </Flex>
+    </Link>
+  );
 };
 
 export default UserPosts;
