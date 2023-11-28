f3bd1b4f4ed8420d86cf72a13185971fe7205505
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-28 12:31:45 +0100

    commit

--- threads-backend/models/postModel.js
+++ threads-backend/models/postModel.js
@@ -1,42 +1,48 @@
 import mongoose from "mongoose";
 
-const postSchema = mongoose.Schema({
-    postedBy: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-    },
-    text: {
-        type: String,
-        maxLength: 500,
-    },
-    img: {
-        type: String,
-    },
-    likes: {
-        type: [mongoose.Schema.Types.ObjectId],
-        ref: "User",
-        required: true,
-    },
-    replies: [
-        {
-            userId: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "User",
-                required: true,
-            },
-            text: {
-                type: String,
-                required: true,
-            },
-            userProfilePic: {
-                type: String,
-            },
-            username: {
-                type: String,
-            },
+const postSchema = mongoose.Schema(
+    {
+        postedBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
         },
-    ],
-});
+        text: {
+            type: String,
+            maxLength: 500,
+        },
+        img: {
+            type: String,
+        },
+        likes: {
+            type: [mongoose.Schema.Types.ObjectId],
+            ref: "User",
+            required: true,
+        },
+        replies: [
+            {
+                userId: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: "User",
+                    required: true,
+                },
+                text: {
+                    type: String,
+                    required: true,
+                },
+                userProfilePic: {
+                    type: String,
+                },
+                username: {
+                    type: String,
+                },
+            },
+        ],
+    },
+    {
+        timestamps: true,
+    }
+
+);
 
 const Post = mongoose.model("Post", postSchema);
 
