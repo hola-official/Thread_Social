fd6514cdda3f9a75cc6ed04d8ee4382e4811bdc1
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-28 12:51:36 +0100

    commit

--- threads-backend/middleware/protectRoute.js
+++ threads-backend/middleware/protectRoute.js
@@ -1,9 +1,18 @@
 import jwt from "jsonwebtoken";
+import User from '../models/userModel'
 
 const protectRoute = async (req, res, next) => {
     try {
         const token = req.cookies.jwt;
-        if(!token) return res.status(401).json({message: 'Unauthorized'})
+        if (!token) return res.status(401).json({ message: 'Unauthorized' })
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        const user = await User.findById(decoded.userId).select('-password')
+
+        req.user = user
+
+        next()
     } catch (err) {
         res.status(500).json({ message: err.message })
         console.log(`Error while signing up user: `, err.message);
