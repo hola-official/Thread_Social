abb7d3cb720164f62402ac0f250d912e7796b851
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-28 12:28:10 +0100

    commit

--- threads-backend/models/postModel.js
+++ threads-backend/models/postModel.js
@@ -1,33 +1,43 @@
 import mongoose from "mongoose";
 
-const postSchema = mongoose.Schema(
-    {
-        postedBy: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User"
+const postSchema = mongoose.Schema({
+    postedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+    text: {
+        type: String,
+        maxLength: 500,
+    },
+    img: {
+        type: String,
+    },
+    likes: {
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: "User",
+        required: true,
+    },
+    replies: [
+        {
+            userId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User",
+                required: true,
+            },
+            text: {
+                type: String,
+                required: true,
+            },
+            userProfilePic: {
+                type: String,
+            },
+            username: {
+                type: String,
+            },
         },
-        text: {
-            type: String,
-            maxLength: 500
-        },
-        img: {
-            type: String
-        },
-        likes: {
-            type: [mongoose.Schema.Types.ObjectId],
-            ref: "User",
-            required: true
-        },
-        replies: [
-            {
-                userId: {
-                    
-                }
-            }
-        ]
-    }
-)
+    ],
+});
 
-const Post  = mongoose.model('Post', postSchema)
+const Post = mongoose.model("Post", postSchema);
 
-export default Post
+export default Post;
