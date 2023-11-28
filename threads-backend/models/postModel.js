03a59937016704706890ba0b8188e92eac1011f2
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-28 12:22:36 +0100

    commit

--- threads-backend/models/postModel.js
+++ threads-backend/models/postModel.js
@@ -2,7 +2,21 @@ import mongoose from "mongoose";
 
 const postSchema = mongoose.Schema(
     {
-        
+        postedBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        text: {
+            type: String,
+            maxLength: 500
+        },
+        img: {
+            type: String
+        },
+        likes: {
+            type: [mongoose.Schema.Types.ObjectId],
+            
+        }
     }
 )
 
