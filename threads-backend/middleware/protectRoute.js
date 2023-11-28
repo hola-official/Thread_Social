08c3bf155c3c2cc50ee6a8699ad9775935c064e4
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-28 12:46:33 +0100

    commit

--- threads-backend/middleware/protectRoute.js
+++ threads-backend/middleware/protectRoute.js
@@ -1,6 +1,9 @@
+import jwt from "jsonwebtoken";
+
 const protectRoute = async (req, res, next) => {
     try {
-
+        const token = req.cookies.jwt;
+        if(!token) return res.status(401).json({message: 'Unauthorized'})
     } catch (err) {
         res.status(500).json({ message: err.message })
         console.log(`Error while signing up user: `, err.message);
