8aa8c3f7f5b8dec58f136f9c5a3a5a1713e852c1
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-28 15:04:40 +0100

    commit

--- threads-backend/controller/userController.js
+++ threads-backend/controller/userController.js
@@ -3,7 +3,7 @@ const User = require('../models/userModel')
 const signUpUser = async (req, res) => {
     try {
         const {name, email, username, password} = req.body
-        const user = await User.findOne({})
+        const user = await User.findOne({$or: [{email},{username}]})
     } catch (err) {
         
     }
