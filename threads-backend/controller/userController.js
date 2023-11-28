326aac5d40410530f0d0526b0fbc93c2df5b2965
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-28 15:06:02 +0100

    commit

--- threads-backend/controller/userController.js
+++ threads-backend/controller/userController.js
@@ -4,6 +4,10 @@ const signUpUser = async (req, res) => {
     try {
         const { name, email, username, password } = req.body
         const user = await User.findOne({ $or: [{ email }, { username }] })
+
+        if(user) {
+            return res.status(400). json({message: 'User already exists'})
+        }
     } catch (err) {
 
     }
