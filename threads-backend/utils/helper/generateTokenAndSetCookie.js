d1ccd56dd75711d73b77a7f6c376e79d1e47a6c2
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-28 16:17:19 +0100

    commit

---
+++ threads-backend/utils/helper/generateTokenAndSetCookie.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+const generateTokenAndSetCookie = (userId, res) => {
+    const token = jwt.sign({userId}, process.env.JWT_SECRET, {
+        expiresIn: '15d'
+    })
+
+    res.cookie('jwt', token, {
+        httpOnly: true,  /// more secure
+        maxAge: 15 * 24 * 60 * 1000,  // 15 days
+        sameSite: 'strict', // CSRF
+    })
+
+    return token
+}
+
+module.exports = generateTokenAndSetCookie
