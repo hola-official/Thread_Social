e1ea747685eff9f0d20dcf0af79169c190330eb3
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-28 15:13:33 +0100

    commit

--- threads-backend/controller/userController.js
+++ threads-backend/controller/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/userModel')
+const bcrypt = require('bcryptjs')
 
 const signUpUser = async (req, res) => {
     try {
@@ -8,6 +9,9 @@ const signUpUser = async (req, res) => {
         if (user) {
             return res.status(400).json({ message: 'User already exists' })
         }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt)
     } catch (err) {
 
     }
