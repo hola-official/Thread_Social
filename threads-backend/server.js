9eb37e644992ebf2b640eb7c272bfe0117163266
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-28 09:16:01 +0100

    commit

--- threads-backend/server.js
+++ threads-backend/server.js
@@ -1,11 +1,16 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import cookieParser from 'cookie-parser'
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
-dotenv.config()
+dotenv.config();
+const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json({limit : "50mb"})) // parse  json data inside the req body
-app.use(express.urlencoded({extended: true})) // parse form data inside the req body
-app.use(cookieParser())
+app.use(express.json({ limit: "50mb" })); // parse  json data inside the req body
+app.use(express.urlencoded({ extended: true })); // parse form data inside the req body
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Welcome Home🏡");
+});
