4a2c33b7a9c4e772f181197aa4eb949d501f8392
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-28 09:11:14 +0100

    commit

---
+++ threads-backend/server.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+
+dotenv.config()
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json({limit : "50mb"})) // parse  json data inside the req body
+app.use(express.urlencoded({extended: true})) // parse form data inside the req body
+app.use(cookieParser)
