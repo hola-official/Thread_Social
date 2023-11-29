d1ecd530d9eb29aea8c3a7f1e8c357a774591e0d
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-29 11:45:55 +0100

    commit

--- threads-backend/routes/userRoute.js
+++ threads-backend/routes/userRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { signUpUser, loginUser, logoutUser } = require("../controller/userController");
+const { signUpUser, loginUser, logoutUser, followUnfollowUser } = require("../controller/userController");
 
 const router = express.Router();
 router.post('/signup', signUpUser)
 router.post('/login', loginUser)
 router.post('/logout', logoutUser)
+router.post('/follow/:id', protectRoute, followUnfollowUser) // toggle state (follow/unfollow)
 
 module.exports = router
