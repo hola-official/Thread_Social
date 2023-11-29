e5bec9056a47a5fe2e9776bbf8997ee87846d367
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-29 11:25:10 +0100

    commit

--- threads-backend/controller/userController.js
+++ threads-backend/controller/userController.js
@@ -73,6 +73,7 @@ const loginUser = async (req, res) => {
 const logoutUser = (req, res) => {
     try {
         res.cookie("jwt", '', { maxAge: 1 })
+        res.status(200).json({ message: "User logged out successfully" })
     } catch (err) {
         res.status(500).json({ error: err.message })
         console.log("Error in LogoutUser", err.message);
