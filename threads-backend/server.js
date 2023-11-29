d45c4c4c6a89641c8b0cf210ab5ee159c09b04b1
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-29 15:43:25 +0100

    commit

--- threads-backend/server.js
+++ threads-backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
-const userRoutes = require('./routes/userRoute');
+const userRoutes = require("./routes/userRoute");
 
 dotenv.config();
 const app = express();
@@ -13,12 +13,14 @@ app.use(express.json({ limit: "50mb" })); // parse  json data inside the req bod
 app.use(express.urlencoded({ extended: true })); // parse form data inside the req body
 app.use(cookieParser());
 
-app.use('/api/users', userRoutes)
+app.use("/api/users", userRoutes);
 app.get("/", (req, res) => {
-    res.send("Welcome Home🏡");
+  res.send("Welcome Home🏡");
 });
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
-    }).catch((err) => console.log(err))
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
+  })
+  .catch((err) => console.log(err));
