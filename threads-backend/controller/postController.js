ce1b5f09589717cc743c4bcc361cca8c5193ff13
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-30 14:19:38 +0100

    commit

--- threads-backend/controller/postController.js
+++ threads-backend/controller/postController.js
@@ -88,6 +88,7 @@ const likeUnlikePost = async (req, res) => {
         } else {
             post.likes.push(userId);
             await post.save();
+            res.status(200).json({ message: "User liked successfully" });
         }
     } catch (err) {
         res.status(500).json({ message: err.message });
