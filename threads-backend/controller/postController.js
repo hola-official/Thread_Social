74dc4ca4af8c6be4255128b9646c7814b12f83f2
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-30 12:26:12 +0100

    commit

--- threads-backend/controller/postController.js
+++ threads-backend/controller/postController.js
@@ -48,7 +48,12 @@ const getPost = async (req, res) => {
 }
 
 const deletePost = async (req, res) => {
-    
+    try {
+        
+    } catch (err) {
+        res.status(500).json({message: err.message})
+        console.log("Error in Delete Post: ", err.message);
+    }
 }
 
 module.exports = {
