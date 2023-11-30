b87ca8a737ad64cd780e14c79cc0faea65a0eb47
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-30 10:13:19 +0100

    commit

--- threads-backend/controller/userController.js
+++ threads-backend/controller/userController.js
@@ -4,179 +4,181 @@ const generateTokenAndSetCookie = require("../utils/helper/generateTokenAndSetCo
 const mongoose = require("mongoose");
 
 const getUserProfile = async (req, res) => {
-    // we fetch the user profile either by username or userId
-    // query is either username or userId
+  // we fetch the user profile either by username or userId
+  // query is either username or userId
 
-    const { query } = req.params;
-    try {
-        let user;
+  const { query } = req.params;
+  try {
+    let user;
 
-        // query is userId
-        if (mongoose.Types.ObjectId.isValid(query)) {
-            user = await User.findOne({ _id: query })
-                .select("-password")
-                .select("-updatedAt");
-        } else {
-            // query is username
-            user = await User.findOne({ username: query })
-                .select("-password")
-                .select("-updatedAt");
-        }
-
-        if (!user)
-            // if user not found
-            return res.status(400).json({ error: "User not found" });
-        res.status(200).json(user);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-        console.log("Error in getUserProfile", err.message);
+    // query is userId
+    if (mongoose.Types.ObjectId.isValid(query)) {
+      user = await User.findOne({ _id: query })
+        .select("-password")
+        .select("-updatedAt");
+    } else {
+      // query is username
+      user = await User.findOne({ username: query })
+        .select("-password")
+        .select("-updatedAt");
     }
+
+    if (!user)
+      // if user not found
+      return res.status(400).json({ error: "User not found" });
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+    console.log("Error in getUserProfile", err.message);
+  }
 };
 
 const signUpUser = async (req, res) => {
-    try {
-        const { name, email, username, password } = req.body;
-        const user = await User.findOne({ $or: [{ email }, { username }] });
+  try {
+    const { name, email, username, password } = req.body;
+    const user = await User.findOne({ $or: [{ email }, { username }] });
 
-        if (user) {
-            return res.status(400).json({ message: "User already exists" });
-        }
-
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-        const newUser = new User({
-            name,
-            email,
-            username,
-            password: hashedPassword,
-        });
-
-        await newUser.save();
-
-        if (newUser) {
-            generateTokenAndSetCookie(newUser._id, res);
-            res.status(201).json({
-                _id: newUser._id,
-                name: newUser.name,
-                email: newUser.email,
-                username: newUser.username,
-            });
-        } else {
-            res.status(400).json({ message: "Invalid user data" });
-        }
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-        console.log("Error in SignupUser: ", err.message);
+    if (user) {
+      return res.status(400).json({ message: "User already exists" });
     }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = new User({
+      name,
+      email,
+      username,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    if (newUser) {
+      generateTokenAndSetCookie(newUser._id, res);
+      res.status(201).json({
+        _id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+        username: newUser.username,
+      });
+    } else {
+      res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+    console.log("Error in SignupUser: ", err.message);
+  }
 };
 
 const loginUser = async (req, res) => {
-    try {
-        const { username, password } = req.body;
-        const user = await User.findOne({ username: username });
-        const isPasswordCorrect = await bcrypt.compare(
-            password,
-            user?.password || ""
-        );
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username: username });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
 
-        if (!user || !isPasswordCorrect)
-            return res.status(400).json({ error: "Invalid username or password" });
+    if (!user || !isPasswordCorrect)
+      return res.status(400).json({ error: "Invalid username or password" });
 
-        if (user.isFrozen) {
-            user.isFrozen = false;
-            await user.save();
-        }
-
-        generateTokenAndSetCookie(user._id, res);
-
-        res.status(200).json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            username: user.username,
-            bio: user.bio,
-            profilePic: user.profilePic,
-        });
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-        console.log("Error in LoginUser", err.message);
+    if (user.isFrozen) {
+      user.isFrozen = false;
+      await user.save();
     }
+
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      username: user.username,
+      bio: user.bio,
+      profilePic: user.profilePic,
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    console.log("Error in LoginUser", err.message);
+  }
 };
 
 const logoutUser = (req, res) => {
-    try {
-        res.cookie("jwt", "", { maxAge: 1 });
-        res.status(200).json({ message: "User logged out successfully" });
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-        console.log("Error in LogoutUser", err.message);
-    }
+  try {
+    res.cookie("jwt", "", { maxAge: 1 });
+    res.status(200).json({ message: "User logged out successfully" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    console.log("Error in LogoutUser", err.message);
+  }
 };
 
 const followUnfollowUser = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const userToModify = await User.findById(id);
-        const currentUser = await User.findById(req.user._id);
+  try {
+    const { id } = req.params;
+    const userToModify = await User.findById(id);
+    const currentUser = await User.findById(req.user._id);
 
-        if (id === req.user._id.toString())
-            return res
-                .status(400)
-                .json({ error: "You can not follow/unfollow yourself" });
-        if (!userToModify || !currentUser)
-            return res.status(400).json({ error: "User not Found" });
+    if (id === req.user._id.toString())
+      return res
+        .status(400)
+        .json({ error: "You can not follow/unfollow yourself" });
+    if (!userToModify || !currentUser)
+      return res.status(400).json({ error: "User not Found" });
 
-        const isFollowing = currentUser.following.includes(id);
-        if (isFollowing) {
-            // Unfollow  user
-            await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
-            await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
-            res.status(200).json({ message: "User Unfollowed Successfully" });
-        } else {
-            // Follow user
-            await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } });
-            await User.findByIdAndUpdate(req.user._id, { $push: { following: id } });
-            res.status(200).json({ message: "User followed Successfully" });
-        }
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-        console.log("Error in Follow/Unfollow", err.message);
+    const isFollowing = currentUser.following.includes(id);
+    if (isFollowing) {
+      // Unfollow  user
+      await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
+      await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
+      res.status(200).json({ message: "User Unfollowed Successfully" });
+    } else {
+      // Follow user
+      await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } });
+      await User.findByIdAndUpdate(req.user._id, { $push: { following: id } });
+      res.status(200).json({ message: "User followed Successfully" });
     }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    console.log("Error in Follow/Unfollow", err.message);
+  }
 };
 
 const updateUser = async (req, res) => {
-    const { name, email, username, password, profilePic, bio } = req.body
-    const userId = req.user._id
-    try {
-        let user = await User.findById(userId)
-        if (!user) return res.status(400).json({ message: "User not found" })
+  const { name, email, username, password, profilePic, bio } = req.body;
+  const userId = req.user._id;
+  try {
+    let user = await User.findById(userId);
+    if (!user) return res.status(400).json({ message: "User not found" });
 
-        if (req.params.id !== userId.toString())
-            return res.status(400).json({ message: "You cannot update other user's profile" })
-        if (password) {
-            const salt = await bcrypt.getSalt(10)
-            const hashedPassword = await bcrypt.hash(password, salt)
-            user.password = hashedPassword
-        }
-        user.name = name || user.name
-        user.email = email || user.email
-        user.username = username || user.username
-        user.profilePic = profilePic || user.profilePic
-        user.bio = bio || user.bio
-
-        await user.save()
-        res.status(200).json({ message: "Profile update successfully", user })
-    } catch (err) {
-        res.status(500).json({ error: err.message })
-        console.log('Error in Update User', err.message);
+    if (req.params.id !== userId.toString())
+      return res
+        .status(400)
+        .json({ message: "You cannot update other user's profile" });
+    if (password) {
+      const salt = await bcrypt.getSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      user.password = hashedPassword;
     }
-}
+    user.name = name || user.name;
+    user.email = email || user.email;
+    user.username = username || user.username;
+    user.profilePic = profilePic || user.profilePic;
+    user.bio = bio || user.bio;
+
+    await user.save();
+    res.status(200).json({ message: "Profile update successfully", user });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    console.log("Error in Update User", err.message);
+  }
+};
 
 module.exports = {
-    getUserProfile,
-    signUpUser,
-    loginUser,
-    logoutUser,
-    followUnfollowUser,
-    updateUser,
+  getUserProfile,
+  signUpUser,
+  loginUser,
+  logoutUser,
+  followUnfollowUser,
+  updateUser,
 };
