0e1cc608f18f6172750030a2c087ccf5c197a9d4
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-30 10:55:24 +0100

    commit

--- threads-backend/controller/postController.js
+++ threads-backend/controller/postController.js
@@ -5,14 +5,15 @@ const createPost = async (req, res) => {
         const { postedBy, text, img } = req.body
 
         if (!postedBy || !text) {
-            return res.status(400).json({message: "postedBy and text fields are required"})
+            return res.status(400).json({ message: "postedBy and text fields are required" })
         }
 
-        const user  = await User.findById(postedBy);
+        const user = await User.findById(postedBy);
 
-        
+
     } catch (err) {
-
+        res.status(500).json({ error: err.message })
+        console.log("Error in CreatePost", err.message);
     }
 }
 
