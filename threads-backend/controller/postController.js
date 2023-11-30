66f1bd5b429e271eef97db311019d90669031707
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-30 15:55:11 +0100

    commit

--- threads-backend/controller/postController.js
+++ threads-backend/controller/postController.js
@@ -128,9 +128,20 @@ const replyToPost = async (req, res) => {
 
 const getFeedPost = async (req, res) => {
     try {
-        
+        const userId = req.user._id
+        const user = await User.findById(userId)
+
+        if(!user) {
+            return res.status(404).json({message: "User not found"})
+        }
+
+        const following = user.following
+
+        const feedPosts = await Post.find({postedBy: {$in: following}}).sort({createdAt: -1});
+
+        res.status(200).json({feedPosts})
     } catch (err) {
-        res.status(500).json({message: err.message})
+        res.status(500).json({message: err.message}) // Internal server error
         console.log("Error in Get Feed Post: ", err.message);
     }
 }
