01aa38bb5536d035132153235aec5d91cca644d1
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-30 12:33:21 +0100

    commit

--- threads-backend/controller/postController.js
+++ threads-backend/controller/postController.js
@@ -58,7 +58,7 @@ const deletePost = async (req, res) => {
             return res.status(401).json({ message: "Unauthorized to delete this post" });
         }
         await Post.findByIdAndDelete(req.params.id)
-        res.status(200).json({ message: 'Post deleted' })
+        res.status(200).json({ message: 'Post deleted successfully' })
     } catch (err) {
         res.status(500).json({ message: err.message }) // ineternal server error
         console.log("Error in Delete Post: ", err.message);
