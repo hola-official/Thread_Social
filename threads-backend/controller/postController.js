9c8f9777e6cd0cce1e0a3d1a20bda0f096c40410
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-30 12:06:06 +0100

    commit

--- threads-backend/controller/postController.js
+++ threads-backend/controller/postController.js
@@ -40,6 +40,7 @@ const getPost = async (req, res) => {
         if (!post) {
             return res.status(404).json({ message: "Post not found" })
         }
+        res.status(200).json({post})
     } catch (err) {
         res.status(500).json({ message: err.message })
         console.log("Error in Get Post: ", err.message);
