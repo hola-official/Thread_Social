ae1b34128bae62ddddcf9e36bba0eeaba29e6f09
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-30 11:17:20 +0100

    commit

--- threads-backend/controller/postController.js
+++ threads-backend/controller/postController.js
@@ -23,8 +23,10 @@ const createPost = async (req, res) => {
         }
 
         const newPost = new Post({ postedBy, text, img });
-        
+
         await newPost.save()
+
+        res.status(201).json({message: "Post created successfully", newPost})
     } catch (err) {
         res.status(500).json({ error: err.message });
         console.log("Error in Create Post: ", err.message);
