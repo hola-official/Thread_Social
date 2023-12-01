b46608b37fb03581aeaea52b70c57db6146c94ec
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-01 21:03:28 +0100

    commit

--- threads-backend/routes/userRoutes.js
+++ threads-backend/routes/userRoutes.js
@@ -14,7 +14,7 @@ router.get("/profile/:query", getUserProfile);
 router.post("/signup", signUpUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
-router.post("/follow/:id", protectRoute, followUnfollowUser); // toggle state (follow/unfollow)
+router.post("/follow/:id", protectRoute, followUnfollowUser); // toggle state (follow / unfollow)
 router.post("/update/:id", protectRoute, updateUser); // update user
 
 module.exports = router;
