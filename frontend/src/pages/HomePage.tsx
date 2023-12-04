3ba44c197c9ca0d20f177937fe67c21c3690bb19
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-04 09:54:34 +0100

    commit

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,8 +1,16 @@
+import { Button, Flex } from '@chakra-ui/react'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      <Link to={'/markzuckerberg'}>
+        <Flex w={'full'} justifyContent={'center'} >
+          <Button mx={'auto'}>Visit Profile Page</Button>
+        </Flex>
+      </Link>
+    </div>
   )
 }
 
