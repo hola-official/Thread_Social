61a5a5e4b6c56a72a89d3ffd9ae9b52fd296f857
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-04 11:38:06 +0100

    commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,7 @@ import { mode } from '@chakra-ui/theme-tools'
 import { extendTheme } from '@chakra-ui/theme-utils'
 import { ColorModeScript } from '@chakra-ui/color-mode'
 import { BrowserRouter } from 'react-router-dom'
+import { RecoilRoot } from 'recoil'
 
 const style = {
   global: (props) => ({
@@ -32,11 +33,13 @@ const colors = {
 const theme = extendTheme({ config, style, colors })
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ChakraProvider theme={theme}>
-        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-        <App />
-      </ChakraProvider>
-    </BrowserRouter>
+    <RecoilRoot>
+      <BrowserRouter>
+        <ChakraProvider theme={theme}>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <App />
+        </ChakraProvider>
+      </BrowserRouter>
+    </RecoilRoot>
   </React.StrictMode>,
 )
