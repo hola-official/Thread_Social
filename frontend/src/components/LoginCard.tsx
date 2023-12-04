c134b2be286189435dd95df6976fa3b9284e9e72
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-04 14:11:43 +0100

    commit

--- frontend/src/components/LoginCard.tsx
+++ frontend/src/components/LoginCard.tsx
@@ -25,7 +25,6 @@ export default function LoginCard() {
 
   return (
     <Flex
-      minH={'100vh'}
       align={'center'}
       justify={'center'}>
       <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
