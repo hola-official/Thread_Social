a6cd1ae61299f2e065139915958516fb670615df
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-04 09:36:30 +0100

    commit

--- threads-backend/controller/postController.js
+++ threads-backend/controller/postController.js
@@ -153,7 +153,7 @@ const getUserPosts = async (req, res) => {
     try {
         const user = await User.findOne({ username });
         if (!user) {
-            return res.status(400).json({ message: "User not found" });
+            return res.status(404).json({ message: "User not found" });
         }
         const posts = await Post.find({ postedBy: user._id }).sort({
             createdAt: -1,
