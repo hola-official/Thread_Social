0df4f67f1efba99a8f7f8ccb7c84f10141f4b464
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-04 12:52:54 +0100

    commit

--- frontend/src/hooks/useShowToast.ts
+++ frontend/src/hooks/useShowToast.ts
@@ -1,6 +1,19 @@
 import { useToast } from "@chakra-ui/react";
-import React, {useCallback} from "react";
+import React, { useCallback } from "react";
 
 const useShowToast = () => {
   const toast = useToast()
+
+  const showToast = useCallback(
+    (title, description, status) => {
+      toast: ({
+        title,
+        description,
+        status,
+        duration: 3000,
+        isClosable: true
+      })
+    },
+    return showToast
+  )
 }
