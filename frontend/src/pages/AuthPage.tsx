e158fe6157df0990bcdc3032ae8a1fefb55b9da0
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-04 10:24:30 +0100

    commit

--- frontend/src/pages/AuthPage.tsx
+++ frontend/src/pages/AuthPage.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import LoginCard from '../components/LoginCard'
+import SignupCard from '../components/SignUpCard'
 
 const AuthPage = () => {
-  return <></>
+  return <>"login" ? <LoginCard /> : <SignupCard /></>
 }
 
 export default AuthPage
