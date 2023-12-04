bf78ff308b863245a87bd5f4a6bd919adab1e81a
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-04 11:31:31 +0100

    commit

--- frontend/src/pages/AuthPage.tsx
+++ frontend/src/pages/AuthPage.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { useRecoilValue } from 'recoil'
 import LoginCard from '../components/LoginCard'
 import SignUpCard from '../components/SignUpCard'
 
 const AuthPage = () => {
+  const authScreenState = useRecoilValue(authScreenAtom)
   return <>"login" ? <LoginCard /> : <SignUpCard /></>
 }
 
