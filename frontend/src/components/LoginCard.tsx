7c7166090da93cf432e6dc3f54a6a974b0865c15
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-04 11:59:16 +0100

    commit

--- frontend/src/components/LoginCard.tsx
+++ frontend/src/components/LoginCard.tsx
@@ -16,9 +16,12 @@ import {
 } from '@chakra-ui/react'
 import React, { useState } from 'react'
 import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons'
+import { useRecoilState } from 'recoil'
+import authScreenAtom from '../atoms/authAtoms'
 
 export default function LoginCard() {
   const [showPassword, setShowPassword] = useState(false)
+  const setAuthScreen = useRecoilState(authScreenAtom)
 
   return (
     <Flex
