f001a0adf0cb82b26841f59fed41295ffab5dd99
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-05 12:45:45 +0100

    commit

--- threads-backend/controller/userController.js
+++ threads-backend/controller/userController.js
@@ -60,6 +60,8 @@ const signUpUser = async (req, res) => {
                 name: newUser.name,
                 email: newUser.email,
                 username: newUser.username,
+                bio: newUser.bio,
+                profilePic: newUser.profilePic
             });
         } else {
             res.status(400).json({ message: "Invalid user data" });
