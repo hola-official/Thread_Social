74abc211f8f76b8a3ac771dec351e3fbfc2d0c44
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-05 11:40:23 +0100

    commit

--- frontend/src/components/LoginCard.tsx
+++ frontend/src/components/LoginCard.tsx
@@ -42,14 +42,14 @@ export default function LoginCard() {
         body: JSON.stringify(inputs),
       });
       const data = await res.json();
-      console.log(data);
       
-
+      
       if (data.error) {
         showToast("Error", data.error, "error");
         return;
       }
-
+      console.log(data);
+      
       localStorage.setItem("user-threads", JSON.stringify(data));
       setUser(data);
     } catch (error) {
