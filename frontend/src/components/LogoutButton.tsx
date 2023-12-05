fef9ea7ff68bc49e0ae9f187d0a14f7dfc6152e4
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-05 09:19:11 +0100

    commit

--- frontend/src/components/LogoutButton.tsx
+++ frontend/src/components/LogoutButton.tsx
@@ -1,11 +1,27 @@
 import { Button } from '@chakra-ui/react'
 import React from 'react'
-import {FiLogout} from "react-icons/fi"
+import { AiOutlineLogout } from "react-icons/ai"
+import { useRecoilState } from 'recoil'
+import userAtom from '../atoms/userAtom'
+import useShowToast from '../hooks/useShowToast'
 
 const LogoutButton = () => {
+  const setUser = useRecoilState(userAtom)
+  const showToast = useShowToast()
+
+  const handleLogout = async () => {
+    try {
+      const res = await fetch('api/users/logout', {
+        method: "POST"
+      })
+      
+    } catch (err) {
+      
+    }
+  }
   return (
     <Button position={'fixed'} top={'30px'} right={'30px'} size={'sm'}>
-      <FiLogout size={20} />
+      <AiOutlineLogout size={20} />
     </Button>
   )
 }
