0a7d3b21e64c6203b64dd031f1ed15d673696684
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-05 15:01:08 +0100

    commit

--- frontend/src/hooks/usePreviewImg.ts
+++ frontend/src/hooks/usePreviewImg.ts
@@ -5,7 +5,14 @@ const usePreviewImg = () => {
 
   const handleImageChange = (e) => {
     const file = e.target.files[0]
-    console.log(file);
+    if(file && file.type.startsWith("image/")){
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImgUrl(reader.result);
+      }
+
+      reader.readAsDataURL(file)
+    }
   }
 
   return {handleImageChange, imgUrl}
