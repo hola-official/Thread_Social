a4219f426a1e8a546217a8d3e3fc61ea068cdc95
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-05 15:52:22 +0100

    commit

--- threads-backend/server.js
+++ threads-backend/server.js
@@ -4,11 +4,16 @@ const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/userRoutes");
 const postRoutes = require("./routes/postRoutes");
-
+const cloudinary = require('cloudinary').v2
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAM,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
 
 app.use(express.json({ limit: "50mb" })); // parse  json data inside the req body
 app.use(express.urlencoded({ extended: true })); // parse form data inside the req body
