e24b74a08d41f386db485995682db81e8c0e3e09
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-06 12:34:38 +0100

    commit

--- frontend/src/components/UserHeader.tsx
+++ frontend/src/components/UserHeader.tsx
@@ -11,116 +11,115 @@ import userAtom from "../atoms/userAtom";
 import { Link as RouterLink } from "react-router-dom";
 
 const UserHeader = ({ user }) => {
-    const toast = useToast();
-    const currentUser = useRecoilValue(userAtom)
+  const toast = useToast();
+  const currentUser = useRecoilValue(userAtom);
 
-    const copyURL = () => {
-        const currentURL = window.location.href;
-        navigator.clipboard.writeText(currentURL).then(() => {
-            toast({
-                title: "Success",
-                description: "Profile link copied.",
-                status: "success",
-                duration: 3000,
-                isClosable: true,
-            });
-        });
-    };
-    return (
-        <VStack gap={4} alignItems={"start"}>
-            <Flex justifyContent={"space-between"} w={"full"}>
-                <Box>
-                    <Text fontSize={"2xl"} fontWeight={"bold"}>
-                        {user.name}
-                    </Text>
-                    <Flex gap={2} alignItems={"center"}>
-                        <Text fontSize={"sm"}>{user.username}</Text>
-                        <Text
-                            fontSize={"xs"}
-                            bg={"gray.dark"}
-                            color={"gray.light"}
-                            p={1}
-                            borderRadius={"full"}
-                        >
-                            threads.net
-                        </Text>
-                    </Flex>
-                </Box>
-                <Box>
-                    {user.profilePic && (
-                        <Avatar
-                            name={user.name}
-                            src={user.profilePic}
-                            size={{ base: "md", md: "xl" }}
-                        />
-                    )}
-                    {!user.profilePic && (
-                        <Avatar
-                            name={user.name}
-                            src="https://tinyurl.com/5ckj7r6d"
-                            size={{ base: "md", md: "xl" }}
-                        />
-                    )}
-                </Box>
-            </Flex>
-            <Text>{user.bio}</Text>
+  const copyURL = () => {
+    const currentURL = window.location.href;
+    navigator.clipboard.writeText(currentURL).then(() => {
+      toast({
+        title: "Success",
+        description: "Profile link copied.",
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+      });
+    });
+  };
+  return (
+    <VStack gap={4} alignItems={"start"}>
+      <Flex justifyContent={"space-between"} w={"full"}>
+        <Box>
+          <Text fontSize={"2xl"} fontWeight={"bold"}>
+            {user.name}
+          </Text>
+          <Flex gap={2} alignItems={"center"}>
+            <Text fontSize={"sm"}>{user.username}</Text>
+            <Text
+              fontSize={"xs"}
+              bg={"gray.dark"}
+              color={"gray.light"}
+              p={1}
+              borderRadius={"full"}
+            >
+              threads.net
+            </Text>
+          </Flex>
+        </Box>
+        <Box>
+          {user.profilePic && (
+            <Avatar
+              name={user.name}
+              src={user.profilePic}
+              size={{ base: "md", md: "xl" }}
+            />
+          )}
+          {!user.profilePic && (
+            <Avatar
+              name={user.name}
+              src="https://tinyurl.com/5ckj7r6d"
+              size={{ base: "md", md: "xl" }}
+            />
+          )}
+        </Box>
+      </Flex>
+      <Text>{user.bio}</Text>
 
-            {currentUser._id === user._id && (
+      {currentUser._id === user._id && (
+        <Link as={RouterLink} to="/update">
+          <Button size={"sm"}>Update Profile</Button>
+        </Link>
+      )}
 
-                <Link as={RouterLink} to='/update'>
-                    <Button size={'sm'}>Update Profile</Button>
-                </Link>
-            )}
-
-            <Flex w={"full"} justifyContent={"space-between"}>
-                <Flex gap={2} alignItems={"center"}>
-                    <Text color={"gray.light"}>{user.followers.length}</Text>
-                    <Box w={1} h={1} bg={"gray.light"} borderRadius="%50"></Box>
-                    <Link color={"gray.light"}>instagram.com</Link>
-                </Flex>
-                <Flex gap={2}>
-                    <Box className="icon-container">
-                        <BsInstagram cursor={"pointer"} size={24} />
-                    </Box>
-                    <Box className="icon-container">
-                        <Menu>
-                            <MenuButton>
-                                <CgMoreO cursor={"pointer"} size={24} />
-                            </MenuButton>
-                            <Portal>
-                                <MenuList bg={"gray.dark"}>
-                                    <MenuItem bg={"gray.dark"} color={"#fff"} onClick={copyURL}>
-                                        Click here
-                                    </MenuItem>
-                                </MenuList>
-                            </Portal>
-                        </Menu>
-                    </Box>
-                </Flex>
-            </Flex>
-            <Flex w={"full"}>
-                <Flex
-                    flex={1}
-                    borderBottom={"1.5px solid white"}
-                    justifyContent={"center"}
-                    pb={"3"}
-                    cursor={"pointer"}
-                >
-                    <Text fontWeight={"bold"}>Thread</Text>
-                </Flex>
-                <Flex
-                    flex={1}
-                    borderBottom={"1px solid gray"}
-                    justifyContent={"center"}
-                    pb={"3"}
-                    color={"gray.light"}
-                    cursor={"pointer"}
-                >
-                    <Text fontWeight={"bold"}>Replies</Text>
-                </Flex>
-            </Flex>
-        </VStack>
-    );
+      <Flex w={"full"} justifyContent={"space-between"}>
+        <Flex gap={2} alignItems={"center"}>
+          <Text color={"gray.light"}>{user.followers.length}</Text>
+          <Box w={1} h={1} bg={"gray.light"} borderRadius="%50"></Box>
+          <Link color={"gray.light"}>instagram.com</Link>
+        </Flex>
+        <Flex gap={2}>
+          <Box className="icon-container">
+            <BsInstagram cursor={"pointer"} size={24} />
+          </Box>
+          <Box className="icon-container">
+            <Menu>
+              <MenuButton>
+                <CgMoreO cursor={"pointer"} size={24} />
+              </MenuButton>
+              <Portal>
+                <MenuList bg={"gray.dark"}>
+                  <MenuItem bg={"gray.dark"} color={"#fff"} onClick={copyURL}>
+                    Click here
+                  </MenuItem>
+                </MenuList>
+              </Portal>
+            </Menu>
+          </Box>
+        </Flex>
+      </Flex>
+      <Flex w={"full"}>
+        <Flex
+          flex={1}
+          borderBottom={"1.5px solid white"}
+          justifyContent={"center"}
+          pb={"3"}
+          cursor={"pointer"}
+        >
+          <Text fontWeight={"bold"}>Thread</Text>
+        </Flex>
+        <Flex
+          flex={1}
+          borderBottom={"1px solid gray"}
+          justifyContent={"center"}
+          pb={"3"}
+          color={"gray.light"}
+          cursor={"pointer"}
+        >
+          <Text fontWeight={"bold"}>Replies</Text>
+        </Flex>
+      </Flex>
+    </VStack>
+  );
 };
 
 export default UserHeader;
