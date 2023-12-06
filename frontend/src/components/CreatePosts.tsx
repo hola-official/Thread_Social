8078c8388acedd2580690eb8dfe91d991845e26d
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-06 15:50:13 +0100

    commit

--- frontend/src/components/CreatePosts.tsx
+++ frontend/src/components/CreatePosts.tsx
@@ -1,14 +1,51 @@
-import { Button } from '@chakra-ui/react'
-import React from 'react'
+import React from "react";
+import { AddIcon } from "@chakra-ui/icons";
+import {
+  Button,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
 
 const CreatePosts = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const finalRef = React.useRef(null);
   return (
     <>
-      <Button>
-          Post
+      <Button
+        position={"fixed"}
+        bottom={10}
+        right={10}
+        leftIcon={<AddIcon />}
+        bg={"gray"}
+      >
+        Post
       </Button>
-    </>
-  )
-}
 
-export default CreatePosts
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Lorem count={2} />
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default CreatePosts;
