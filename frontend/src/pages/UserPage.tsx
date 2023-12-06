0d7db49841bd91cf4691ceda17e82c3f3f7eaac6
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-06 10:58:42 +0100

    commit

--- frontend/src/pages/UserPage.tsx
+++ frontend/src/pages/UserPage.tsx
@@ -4,13 +4,23 @@ import UserPosts from '../components/UserPosts'
 
 const UserPage = () => {
   const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const res = await fetch()
+      } catch (error) {
+        
+      }
+    }
+  }, [username])
   return (
     <>
-      <UserHeader  />
+      <UserHeader />
       <UserPosts likes={200} replies={50} postImg={'/post1.png'} postTitle={'What did you think about this guy'} postTime={'1d'} />
       <UserPosts likes={50} replies={10} postImg={'/post2.png'} postTitle={'This is great!!'} postTime={'5d'} />
       <UserPosts likes={10} replies={20} postImg={'/post3.png'} postTitle={'Wow! awesome'} postTime={'8d'} />
-      
+
     </>
   )
 }
