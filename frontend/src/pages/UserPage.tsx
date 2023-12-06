397d186c3d824215f59ff1a7c1f135f833750245
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-06 11:05:12 +0100

    commit

--- frontend/src/pages/UserPage.tsx
+++ frontend/src/pages/UserPage.tsx
@@ -1,18 +1,22 @@
 import React, { useState, useEffect } from 'react'
 import UserHeader from '../components/UserHeader'
 import UserPosts from '../components/UserPosts'
+import { useParams } from 'react-router-dom'
 
 const UserPage = () => {
   const [user, setUser] = useState(null)
+  const { username } = useParams()
 
   useEffect(() => {
     const getUser = async () => {
       try {
         const res = await fetch(`/api/users/profile/${username}`)
         const data = await res.json()
+        console.log(data);
+
       } catch (error) {
         console.error(error);
-        
+
       }
     }
   }, [username])
