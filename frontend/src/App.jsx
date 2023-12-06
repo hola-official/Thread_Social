95ec7090d1e84f238616403cad86218549d00496
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-06 15:40:11 +0100

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import { useRecoilValue } from 'recoil'
 import userAtom from './atoms/userAtom'
 import LogoutButton from './components/LogoutButton'
 import UpdateProfilePage from './pages/UpdateProfilePage'
+import CreatePosts from './components/CreatePosts'
 
 function App() {
   const user = useRecoilValue(userAtom)
@@ -26,6 +27,7 @@ function App() {
 
       </Routes>
         {user && <LogoutButton/>}
+        {user && <CreatePosts/>}
     </Container>
   )
 }
