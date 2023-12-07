4a4c58917b26825d302bf8c92746a0c85bf93128
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-07 11:09:16 +0100

    commit

--- frontend/src/components/CreatePosts.tsx
+++ frontend/src/components/CreatePosts.tsx
@@ -47,7 +47,12 @@ const CreatePosts = () => {
   const handleCreatePost = async () => {
     setLoading(true)
     try {
-
+      const res = await fetch('/api/posts/create',{
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
     } catch (error) {
 
     } finally {
