b4ed25ca89f2f79208005d961fb6de068942cbac
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-07 09:21:01 +0100

    commit

--- frontend/src/components/CreatePosts.tsx
+++ frontend/src/components/CreatePosts.tsx
@@ -42,7 +42,6 @@ const CreatePosts = () => {
             <Button colorScheme="blue" mr={3} onClick={onClose}>
               Close
             </Button>
-            <Button variant="ghost">Secondary Action</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
