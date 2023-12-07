7b5dc8a3a78b5f6ebb0248b6bcfd746d556b045f
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-07 14:43:35 +0100

    commit

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,13 +1,14 @@
-import { Flex, Image, useColorMode } from "@chakra-ui/react"
 import React from "react"
+import { useRecoilValue } from "recoil"
+import { Flex, Image, Link, useColorMode } from "@chakra-ui/react"
 
 
 const Header = () => {
-    const { colorMode, toggleColorMode } = useColorMode()
+        const { colorMode, toggleColorMode } = useColorMode()
 
-    return  <Flex justifyContent={"center"} mt={6} mb="12">
-            <Image alt="logo"
-                w={6} src={colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"} onClick={toggleColorMode} cursor={"pointer"} />
+        return <Flex justifyContent={"center"} mt={6} mb="12">
+                <Image alt="logo"
+                        w={6} src={colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"} onClick={toggleColorMode} cursor={"pointer"} />
         </Flex>
 }
 
