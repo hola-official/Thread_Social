03e7cdb6ae955b94fa6294ebbe2d2738862702cc
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-07 14:48:52 +0100

    commit

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,18 +1,24 @@
-import React from "react"
-import { useRecoilValue } from "recoil"
-import { AiFillHome } from 'react-icons/ai'
-import { Link as RouterLink } from "react-router-dom"
+import React from "react";
+import { useRecoilValue } from "recoil";
+import { AiFillHome } from "react-icons/ai";
+import { Link as RouterLink } from "react-router-dom";
 // import RxAvartar from './RxAvatar'
-import { Flex, Image, Link, useColorMode } from "@chakra-ui/react"
-
+import { Flex, Image, Link, useColorMode } from "@chakra-ui/react";
 
 const Header = () => {
-        const { colorMode, toggleColorMode } = useColorMode()
+        const { colorMode, toggleColorMode } = useColorMode();
 
-        return <Flex justifyContent={"center"} mt={6} mb="12">
-                <Image alt="logo"
-                        w={6} src={colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"} onClick={toggleColorMode} cursor={"pointer"} />
-        </Flex>
-}
+        return (
+                <Flex justifyContent={"center"} mt={6} mb="12">
+                        <Image
+                                alt="logo"
+                                w={6}
+                                src={colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"}
+                                onClick={toggleColorMode}
+                                cursor={"pointer"}
+                        />
+                </Flex>
+        );
+};
 
-export default Header
+export default Header;
