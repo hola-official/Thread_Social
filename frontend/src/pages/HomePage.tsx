04b176f369cfc509bf8f6cc23a38c1e3e9c1c35e
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-08 10:22:55 +0100

    commit

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,8 +1,24 @@
 import { Button, Flex } from '@chakra-ui/react'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import useShowToast from '../hooks/useShowToast'
 
 const HomePage = () => {
+  const showToast = useShowToast()
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    const getFeedPosts = async () => {
+      try {
+        const res = await fetch('/api/posts/feed');
+        const data = await res.json();
+      } catch (error) {
+        console.log(error);
+        
+        showToast("Error", error, "error")
+      }
+    }
+  }, [])
+  
   return (
     <div>
       <Link to={'/markzuckerberg'}>
