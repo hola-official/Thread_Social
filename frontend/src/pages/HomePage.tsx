51fd9ad1a0d4d3511f3ca1d7b31d773a76aad7a0
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-08 10:58:23 +0100

    commit

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { Button, Flex } from "@chakra-ui/react";
+import { Button, Flex, Spinner, Text } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import useShowToast from "../hooks/useShowToast";
 
 const HomePage = () => {
+  const [post, setPost] = useState([])
   const showToast = useShowToast();
   const [loading, setLoading] = useState(true);
 
@@ -15,7 +16,10 @@ const HomePage = () => {
         const data = await res.json();
         if (data.error) {
           showToast("Error", data.error, "error");
+          return;
         }
+        console.log(data);
+        
       } catch (error) {
         console.log(error);
 
@@ -24,17 +28,22 @@ const HomePage = () => {
         setLoading(false);
       }
     };
+    getFeedPosts();
   }, [showToast]);
 
-  return (
-    <div>
-      <Link to={"/markzuckerberg"}>
-        <Flex w={"full"} justifyContent={"center"}>
-          <Button mx={"auto"}>Visit Profile Page</Button>
-        </Flex>
-      </Link>
-    </div>
-  );
+return (
+  <>
+  {!loading && post.length === 0 && (
+    <Text>Pls follow a user to see feed posts</Text>
+  )}
+  
+  {loading && (
+    <Flex justifyContent={'center'}>
+      <Spinner size={'xl'}/>
+    </Flex>
+  )}
+  </>
+)
 };
 
 export default HomePage;
