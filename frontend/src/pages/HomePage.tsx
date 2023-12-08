3a6713a46d4b4cd75a5192183b66b1ef37730e9c
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-08 10:26:14 +0100

    commit

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,33 +1,40 @@
-import { Button, Flex } from '@chakra-ui/react'
-import React from 'react'
-import { Link } from 'react-router-dom'
-import useShowToast from '../hooks/useShowToast'
+import React, { useEffect, useState } from "react";
+import { Button, Flex } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import useShowToast from "../hooks/useShowToast";
 
 const HomePage = () => {
-  const showToast = useShowToast()
-  const [loading, setLoading] = useState(true)
+  const showToast = useShowToast();
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     const getFeedPosts = async () => {
+      setLoading(true);
       try {
-        const res = await fetch('/api/posts/feed');
+        const res = await fetch("/api/posts/feed");
         const data = await res.json();
+        if (data.error) {
+          showToast("Error", data.error, "error");
+        }
       } catch (error) {
         console.log(error);
-        
-        showToast("Error", error, "error")
+
+        showToast("Error", error, "error");
+      } finally {
+        setLoading(false);
       }
-    }
-  }, [])
-  
+    };
+  }, [showToast]);
+
   return (
     <div>
-      <Link to={'/markzuckerberg'}>
-        <Flex w={'full'} justifyContent={'center'}  >
-          <Button mx={'auto'}>Visit Profile Page</Button>
+      <Link to={"/markzuckerberg"}>
+        <Flex w={"full"} justifyContent={"center"}>
+          <Button mx={"auto"}>Visit Profile Page</Button>
         </Flex>
       </Link>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
