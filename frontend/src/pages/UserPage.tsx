be12ea74879d8a253e38f41804e108389cb1c4fb
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-11 16:02:54 +0100

    commit

--- frontend/src/pages/UserPage.tsx
+++ frontend/src/pages/UserPage.tsx
@@ -62,6 +62,10 @@ const UserPage = () => {
   return (
     <>
       <UserHeader user={user} />
+      {!fetchingPosts && posts.length === 0 && <h1>User has no posts</h1>}
+      {fetchingPosts && (<Flex justifyContent={'center'} my={12} >
+        <Spinner size={'xl'} />
+      </Flex>)}
       {/* <UserPosts likes={200} replies={50} postImg={'/post1.png'} postTitle={'What did you think about this guy'} postTime={'1d'} />
       <UserPosts likes={50} replies={10} postImg={'/post2.png'} postTitle={'This is great!!'} postTime={'5d'} />
       <UserPosts likes={10} replies={20} postImg={'/post3.png'} postTitle={'Wow! awesome'} postTime={'8d'} /> */}
