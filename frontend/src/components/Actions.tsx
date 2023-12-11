43f6ad79a168a6baf8adebc7752857eae796c9f7
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-11 10:31:29 +0100

    commit

--- frontend/src/components/Actions.tsx
+++ frontend/src/components/Actions.tsx
@@ -5,8 +5,8 @@ import userAtom from "../atoms/userAtom";
 
 const Actions = ({ post: post_ }) => {
   const user = useRecoilValue(userAtom)
-  const [liked, setLiked] = useState(post.likes.includes(user?._id))
   const [post, setPost] = useState(post_)
+  const [liked, setLiked] = useState(post.likes.includes(user?._id))
 
   return (
     <>
