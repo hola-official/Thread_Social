cf96dd1de8ae64339bb8b0d6faa61bdae849d1df
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-11 15:12:11 +0100

    commit

--- frontend/src/pages/UserPage.tsx
+++ frontend/src/pages/UserPage.tsx
@@ -1,21 +1,21 @@
-import React, { useState, useEffect } from 'react'
-import UserHeader from '../components/UserHeader'
-import UserPosts from '../components/UserPosts'
-import { useParams } from 'react-router-dom'
-import useShowToast from '../hooks/useShowToast'
-import { Flex, Spinner } from '@chakra-ui/react'
+import React, { useState, useEffect } from "react";
+import UserHeader from "../components/UserHeader";
+import UserPosts from "../components/UserPosts";
+import { useParams } from "react-router-dom";
+import useShowToast from "../hooks/useShowToast";
+import { Flex, Spinner } from "@chakra-ui/react";
 
 const UserPage = () => {
-  const [user, setUser] = useState(null)
-  const { username } = useParams()
-  const showToast = useShowToast()
-  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState(null);
+  const { username } = useParams();
+  const showToast = useShowToast();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getUser = async () => {
       try {
-        const res = await fetch(`/api/users/profile/${username}`)
-        const data = await res.json()
+        const res = await fetch(`/api/users/profile/${username}`);
+        const data = await res.json();
 
         if (data.error) {
           showToast("Error", data.error, "error");
@@ -23,31 +23,33 @@ const UserPage = () => {
         }
         // console.log(data);
 
-        setUser(data)
+        setUser(data);
       } catch (error) {
-        showToast("Error", error, "error")
-
+        showToast("Error", error, "error");
       } finally {
-        setLoading(false)
+        setLoading(false);
       }
-    }
+    };
     getUser();
-  }, [username, showToast])
+  }, [username, showToast]);
 
   const getPosts = async () => {
     try {
-
+      const res = await fetch(`/api/posts/user/${username}`);
+      const data = await res.json();
+      console.log(data);
+      setPosts(data);
     } catch (error) {
-      showToast("Error", error, "error")
+      showToast("Error", error, "error");
     }
-  }
+  };
 
   if (!user && loading) {
     return (
-      <Flex justifyContent={'center'} >
-        <Spinner size={'xl'} />
+      <Flex justifyContent={"center"}>
+        <Spinner size={"xl"} />
       </Flex>
-    )
+    );
   }
   return (
     <>
@@ -55,9 +57,8 @@ const UserPage = () => {
       {/* <UserPosts likes={200} replies={50} postImg={'/post1.png'} postTitle={'What did you think about this guy'} postTime={'1d'} />
       <UserPosts likes={50} replies={10} postImg={'/post2.png'} postTitle={'This is great!!'} postTime={'5d'} />
       <UserPosts likes={10} replies={20} postImg={'/post3.png'} postTitle={'Wow! awesome'} postTime={'8d'} /> */}
-
     </>
-  )
-}
+  );
+};
 
-export default UserPage
+export default UserPage;
