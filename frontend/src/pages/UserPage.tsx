24186dc3e83a088574f3919efaabe26a52ee3623
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-11 15:02:55 +0100

    commit

--- frontend/src/pages/UserPage.tsx
+++ frontend/src/pages/UserPage.tsx
@@ -44,9 +44,9 @@ const UserPage = () => {
   return (
     <>
       <UserHeader user={user} />
-      <UserPosts likes={200} replies={50} postImg={'/post1.png'} postTitle={'What did you think about this guy'} postTime={'1d'} />
+      {/* <UserPosts likes={200} replies={50} postImg={'/post1.png'} postTitle={'What did you think about this guy'} postTime={'1d'} />
       <UserPosts likes={50} replies={10} postImg={'/post2.png'} postTitle={'This is great!!'} postTime={'5d'} />
-      <UserPosts likes={10} replies={20} postImg={'/post3.png'} postTitle={'Wow! awesome'} postTime={'8d'} />
+      <UserPosts likes={10} replies={20} postImg={'/post3.png'} postTitle={'Wow! awesome'} postTime={'8d'} /> */}
 
     </>
   )
