d6088f4c83d8ba0db0002be5c3c8a866ea21ae7c
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-11 15:07:37 +0100

    commit

--- threads-backend/routes/postRoutes.js
+++ threads-backend/routes/postRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router()
 
 router.get("/feed", protectRoute, getFeedPost)
 router.get("/:id", getPost)
+router.get("/user/:username", getUserPosts)
 router.post("/create", protectRoute, createPost)
 router.delete("/:id", protectRoute, deletePost)
 router.put("/like/:id", protectRoute, likeUnlikePost)
