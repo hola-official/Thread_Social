1f76bfcfa8f9017f91acc6f64928d73fb327ac62
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-12 10:46:58 +0100

    commit

--- frontend/src/pages/PostPage.tsx
+++ frontend/src/pages/PostPage.tsx
@@ -45,7 +45,8 @@ const PostPage = () => {
         showToast("Error", error, "error");
       }
     };
-  }, []);
+    getPosts();
+  }, [showToast, pid]);
 
   if (!user && loading) {
     return (
@@ -63,7 +64,7 @@ const PostPage = () => {
         justifyContent={"space-between"}
       >
         <Flex alignItems={"center"}>
-          <Avatar src={user.profilePic} name="Aliu Musa" size={"md"} mr={2} />
+          <Avatar src={user.profilePic} name={user.name} size={"md"} mr={2} />
           <Text fontSize={"sm"}>{user.username}</Text>
           <Image src="/verified.png" h={4} w={4} ml={2} />
         </Flex>
