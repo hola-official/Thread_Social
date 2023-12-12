29c5a2d0921307e6c60dd92cfefe4a8d65d4e9c6
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-12 09:59:35 +0100

    commit

--- frontend/src/pages/PostPage.tsx
+++ frontend/src/pages/PostPage.tsx
@@ -1,25 +1,50 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from "react";
 import { BsThreeDots } from "react-icons/bs";
-import { Avatar, Box, Button, Divider, Flex, Image, Text } from '@chakra-ui/react'
+import {
+  Avatar,
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Image,
+  Text,
+} from "@chakra-ui/react";
 import { Portal } from "@chakra-ui/portal";
-import { Menu, MenuButton, MenuList, MenuItem, MenuDivider, MenuGroup } from "@chakra-ui/menu";
-import Actions from '../components/Actions';
-import Comments from '../components/Comments';
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  MenuGroup,
+} from "@chakra-ui/menu";
+import Actions from "../components/Actions";
+import Comments from "../components/Comments";
 
 const PostPage = ({ postImg }) => {
   // const [liked, setLiked] = useState(false)
   return (
     <>
-      <Flex w={'full'} gap={3} alignItems={'center'} justifyContent={'space-between'}>
-
-        <Flex alignItems={'center'}>
-          <Avatar src='/aliumusa.jpeg' name='Aliu Musa' size={'md'} mr={2} />
-          <Text fontSize={'sm'}>Aliu Musa</Text>
-          <Image src='/verified.png' h={4} w={4} ml={2} />
+      <Flex
+        w={"full"}
+        gap={3}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+      >
+        <Flex alignItems={"center"}>
+          <Avatar src="/aliumusa.jpeg" name="Aliu Musa" size={"md"} mr={2} />
+          <Text fontSize={"sm"}>Aliu Musa</Text>
+          <Image src="/verified.png" h={4} w={4} ml={2} />
         </Flex>
 
-        <Flex alignItems={'center'} gap={4}>
-          <Text fontSize={{ base: "xs", md: "sm" }} textAlign={'right'} color={'gray.light'} >2day</Text>
+        <Flex alignItems={"center"} gap={4}>
+          <Text
+            fontSize={{ base: "xs", md: "sm" }}
+            textAlign={"right"}
+            color={"gray.light"}
+          >
+            2day
+          </Text>
           {/* <Menu>
             <MenuButton>
               <BsThreeDots cursor={'pointer'} />
@@ -39,15 +64,20 @@ const PostPage = ({ postImg }) => {
         </Flex>
       </Flex>
       <Text my={3}>Hello gbogbo aye!!!</Text>
-      <Box borderRadius={6} overflow={'hidden'} border={'1px solid'} borderColor={'gray.light'}>
-        <Image src='/post1.png' width={'full'} />
+      <Box
+        borderRadius={6}
+        overflow={"hidden"}
+        border={"1px solid"}
+        borderColor={"gray.light"}
+      >
+        <Image src="/post1.png" width={"full"} />
       </Box>
 
       <Flex>
         <Actions liked={liked} setLiked={setLiked} />
       </Flex>
 
-      <Flex color={'gray.light'} gap={2} fontSize={'sm'} alignItems={'center'}>
+      <Flex color={"gray.light"} gap={2} fontSize={"sm"} alignItems={"center"}>
         <Text>200 replies</Text>
         <Box w={0.5} h={0.5} bg={"gray.light"}></Box>
         <Text>{21 + (liked ? 1 : 0)} likes</Text>
@@ -55,10 +85,10 @@ const PostPage = ({ postImg }) => {
 
       <Divider my={4} />
 
-      <Flex justifyContent={'space-between'}>
-        <Flex alignItems={'center'} gap={2}>
-          <Text fontSize={'2xl'}>👋</Text>
-          <Text color={'gray.light'}>Get the app to like, reply and post.</Text>
+      <Flex justifyContent={"space-between"}>
+        <Flex alignItems={"center"} gap={2}>
+          <Text fontSize={"2xl"}>👋</Text>
+          <Text color={"gray.light"}>Get the app to like, reply and post.</Text>
         </Flex>
         <Button>Get</Button>
       </Flex>
@@ -67,7 +97,7 @@ const PostPage = ({ postImg }) => {
 
       {/* <Comments username={'Ryan Florence'} likes={12} createdAt={'2 min ago'} userAvatar={'https://bit.ly/ryan-florence'} comment={"Keep it up!!"} /> */}
     </>
-  )
-}
+  );
+};
 
-export default PostPage
+export default PostPage;
