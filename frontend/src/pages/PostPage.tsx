10ae91f8ddc48e5c543cf7790a6c4fde09fd1f27
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-12 11:17:55 +0100

    commit

--- frontend/src/pages/PostPage.tsx
+++ frontend/src/pages/PostPage.tsx
@@ -56,29 +56,31 @@ const PostPage = () => {
       </Flex>
     );
   }
-  return (
-    <>
-      <Flex
-        w={"full"}
-        gap={3}
-        alignItems={"center"}
-        justifyContent={"space-between"}
-      >
-        <Flex alignItems={"center"}>
-          <Avatar src={user.profilePic} name={user.name} size={"md"} mr={2} />
-          <Text fontSize={"sm"}>{user.username}</Text>
-          <Image src="/verified.png" h={4} w={4} ml={2} />
-        </Flex>
 
-        <Flex alignItems={"center"} gap={4}>
-          <Text
-            fontSize={{ base: "xs", md: "sm" }}
-            textAlign={"right"}
-            color={"gray.light"}
-          >
-            2day
-          </Text>
-          {/* <Menu>
+  if (!post)
+    return (
+      <>
+        <Flex
+          w={"full"}
+          gap={3}
+          alignItems={"center"}
+          justifyContent={"space-between"}
+        >
+          <Flex alignItems={"center"}>
+            <Avatar src={user.profilePic} name={user.name} size={"md"} mr={2} />
+            <Text fontSize={"sm"}>{user.username}</Text>
+            <Image src="/verified.png" h={4} w={4} ml={2} />
+          </Flex>
+
+          <Flex alignItems={"center"} gap={4}>
+            <Text
+              fontSize={{ base: "xs", md: "sm" }}
+              textAlign={"right"}
+              color={"gray.light"}
+            >
+              2day
+            </Text>
+            {/* <Menu>
             <MenuButton>
               <BsThreeDots cursor={'pointer'} />
             </MenuButton>
@@ -94,43 +96,43 @@ const PostPage = () => {
               </MenuGroup>
             </MenuList>
           </Menu> */}
+          </Flex>
         </Flex>
-      </Flex>
-      <Text my={3}>{post.post.text}</Text>
-      <Box
-        borderRadius={6}
-        overflow={"hidden"}
-        border={"1px solid"}
-        borderColor={"gray.light"}
-      >
-        <Image src="/post1.png" width={"full"} />
-      </Box>
+        <Text my={3}>{post.text}</Text>
+        <Box
+          borderRadius={6}
+          overflow={"hidden"}
+          border={"1px solid"}
+          borderColor={"gray.light"}
+        >
+          <Image src={post.img} width={"full"} />
+        </Box>
 
-      <Flex>
-        {/* <Actions liked={liked} setLiked={setLiked} /> */}
-      </Flex>
-
-      <Flex color={"gray.light"} gap={2} fontSize={"sm"} alignItems={"center"}>
-        <Text>200 replies</Text>
-        <Box w={0.5} h={0.5} bg={"gray.light"}></Box>
-        {/* <Text>{21 + (liked ? 1 : 0)} likes</Text> */}
-      </Flex>
-
-      <Divider my={4} />
-
-      <Flex justifyContent={"space-between"}>
-        <Flex alignItems={"center"} gap={2}>
-          <Text fontSize={"2xl"}>👋</Text>
-          <Text color={"gray.light"}>Get the app to like, reply and post.</Text>
+        <Flex>
+          <Actions post={post} />
         </Flex>
-        <Button>Get</Button>
-      </Flex>
 
-      <Divider my={4} />
+        <Flex color={"gray.light"} gap={2} fontSize={"sm"} alignItems={"center"}>
+          <Text>200 replies</Text>
+          <Box w={0.5} h={0.5} bg={"gray.light"}></Box>
+          <Text>{post.likes.length} likes</Text>
+        </Flex>
 
-      {/* <Comments username={'Ryan Florence'} likes={12} createdAt={'2 min ago'} userAvatar={'https://bit.ly/ryan-florence'} comment={"Keep it up!!"} /> */}
-    </>
-  );
+        <Divider my={4} />
+
+        <Flex justifyContent={"space-between"}>
+          <Flex alignItems={"center"} gap={2}>
+            <Text fontSize={"2xl"}>👋</Text>
+            <Text color={"gray.light"}>Get the app to like, reply and post.</Text>
+          </Flex>
+          <Button>Get</Button>
+        </Flex>
+
+        <Divider my={4} />
+
+        {/* <Comments username={'Ryan Florence'} likes={12} createdAt={'2 min ago'} userAvatar={'https://bit.ly/ryan-florence'} comment={"Keep it up!!"} /> */}
+      </>
+    );
 };
 
 export default PostPage;
