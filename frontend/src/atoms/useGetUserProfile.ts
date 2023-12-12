f8d9ff4e4b2b539c271094e3d4765879707f1f1d
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-12-12 10:11:19 +0100

    commit

--- frontend/src/atoms/useGetUserProfile.ts
+++ frontend/src/atoms/useGetUserProfile.ts
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
+import useShowToast from '../hooks/useShowToast'
 
 const useGetUserProfile = () => {
   const [user, setUser] = useState(null)
   const [loading, setLoading] = useState(true)
   const { username } = useParams()
+  const showToast = useShowToast()
 
 }
 
